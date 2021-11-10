a4b9060bbab559c49328b4145726957ae5b9eaad
NogDo <ssb9292@naver.com>
2021-11-11 02:15:46 +0900

    11-11패치
    이동블럭과 부모관계에 대한 코드 개선
    스프링 발판 추가

---
+++ Ragdoll Prac/Assets/Scripts/Object/Spring.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    public GameObject objPlayer;
+    public PlayerController playerController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player_Foot"))
+        {
+            playerController.isGrounded = true;
+            playerController.AddJumpForce();
+        }
+    }
+}
