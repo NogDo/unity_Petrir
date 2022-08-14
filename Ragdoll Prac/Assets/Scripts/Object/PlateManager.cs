7677656fd6b0cce1232a2725cf78003dd1d81453
NogDo <ssb9292@naver.com>
2022-08-14 17:12:20 +0900

    0814
    중간 그래픽과 합치기위해

--- Ragdoll Prac/Assets/Scripts/Object/PlateManager.cs
+++ Ragdoll Prac/Assets/Scripts/Object/PlateManager.cs
@@ -270,6 +270,26 @@ public class PlateManager : MonoBehaviour
                 }
                 break;
 
+            case Stage.Stage1_3:
+                if (material.GetComponent<Image>().sprite == recipeManager.GetStage3RollcakeRecipe()[nMaterialIndex])
+                {
+                    Debug.Log("재료 맞춤");
+                    Sprite currentMaterial = material.GetComponent<Image>().sprite;
+
+                    image_Create.color = Color.white;
+                    image_Create.sprite = recipeManager.GetStage3RollcakeCreate()[nMaterialIndex];
+                    chestManager.list_Material.Remove(recipeManager.GetStage2StrawberryTartRecipe()[nMaterialIndex]);
+
+                    material.SetActive(false);
+                    material.GetComponent<MaterialDrag>().Reset_Ingredient_PositionAndParent();
+                    nMaterialIndex++;
+                    if (nMaterialIndex >= recipeManager.GetStage3RollcakeRecipe().Count)
+                    {
+                        obj_BakeItButton.SetActive(true);
+                    }
+                }
+                break;
+
             default:
                 Debug.Log("스테이지가 정의되지 않은 오븐입니다. (이 오류는 오븐매니저에서 발생했습니다.)");
                 break;
